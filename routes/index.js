58f298b909d8e3684309bb1f22e8870f9e41b690
roflmelon <harryhz508@gmail.com>
2021-02-14 20:14:41 -0500

    added navbar spacer, logo, and ejs views

--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Welcome to Harry\'s Website' });
 });
 
 /* GET home page alternative. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Welcome to Harry\'s Website' });
 });
 
 
 /* GET about me page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about_me', { title: 'About Me' });
 });
 
-/* GET Skills page. */
-router.get('/skill', function(req, res, next) {
-  res.render('index', { title: 'Skill' });
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('my_projects', { title: 'My Projects' });
 });
 
 /* GET service page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services I Provide' });
+  res.render('services', { title: 'Services' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+  res.render('contact_me', { title: 'Contact Me' });
 });
 
 module.exports = router;
